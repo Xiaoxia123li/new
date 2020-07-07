3a0481b6f840c2a4ba1d683518fe9f2dc3c8078f
Xiaoxia Li <lxx5658@gmail.com>
2020-07-06 17:14:49 -0700

    2nd try

---
+++ HelloGit/src/hello/git/Hello1.java
@@ -0,0 +1,8 @@
+package hello.git;
+
+public class Hello1 {
+	public static void main(String[] args) {
+		System.out.println("The second time try: 2020-7-6");
+	}
+
+}
--- HelloGit/src/hello/git/HelloGitConnection.java
+++ HelloGit/src/hello/git/HelloGitConnection.java
@@ -2,4 +2,7 @@ package hello.git;
 
 public class HelloGitConnection {
 
+	public static void main(String[] args) {
+		System.out.println("modified");
+	}
 }
